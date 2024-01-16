639ce277192aacfc494ac0b001a58af7398304fe
sitaraliang <sitaraliang@gmail.com>
2024-01-16 20:40:24 +0100

    update projects.html

--- js/script_cv.js
+++ js/script_cv.js
@@ -1,30 +1,39 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close1 = document.getElementsByClassName("close")[0];
+var close2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {
-  modal.style.display = "block";
+  modal1.style.display = "block";
 }
 
 btn2.onclick = function() {
-    modal.style.display = "block";
+    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+close1.onclick = function() {
+  modal1.style.display = "none";
 }
+
+close2.onclick = function() {
+  modal2.style.display = "none";
+}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
